96b4acd506030fc9515ae8ca02f4c6917fa45ae1
Sinister_30N4 <36511097+tongJK@users.noreply.github.com>
2018-05-03 19:44:09 +0700

    Update i2c.ino

--- i2c.ino
+++ i2c.ino
@@ -1,49 +1,37 @@
 #include <Wire.h>
 
+//Slave Address for the Communication
 #define SLAVE_ADDRESS 0x04
-int number = 0;
+
+char number[50];
 int state = 0;
 
+//Code Initialization
 void setup() {
-pinMode(13, OUTPUT);
-Serial.begin(9600); // start serial for output
-// initialize i2c as slave
-Wire.begin(SLAVE_ADDRESS);
-
-// define callbacks for i2c communication
-Wire.onReceive(receiveData);
-Wire.onRequest(sendData);
-
-Serial.println(“Ready!”);
+  // initialize i2c as slave
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  //  Wire.onRequest(sendData);
 }
 
 void loop() {
-delay(100);
-}
+  delay(100);
+} // end loop
 
 // callback for received data
-void receiveData(int byteCount){
-
-while(Wire.available()) {
-number = Wire.read();
-Serial.print(“data received: “);
-Serial.println(number);
-
-if (number == 1){
-
-if (state == 0){
-digitalWrite(13, HIGH); // set the LED on
-state = 1;
-}
-else{
-digitalWrite(13, LOW); // set the LED off
-state = 0;
-}
-}
-}
-}
+void receiveData(int byteCount) {
+  int i = 0;
+  while (Wire.available()) {
+    number[i] = Wire.read();
+    i++;
+  }
+  number[i] = '\0';
+  Serial.print(number);
+}  // end while
 
 // callback for sending data
-void sendData(){
-Wire.write(number);
+void sendData() {
+  Wire.write(number);
 }
